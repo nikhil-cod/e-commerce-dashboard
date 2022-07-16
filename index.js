4cc0a2c7c6bff3b7da1781d76041cfdbc5fd0029
Nikhil Chakole <nikhilchakole1997@gmail.com>
2022-07-16 14:04:43 +0530

    React setup

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
+const app = express();
+
+app.get('/',(req, resp)=>{
+    resp.send("Success");
+})
+
+app.listen(5000);
